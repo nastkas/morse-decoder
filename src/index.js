402adf5a3e5de18bd7db1916f4eea98630aa010f
nastkas <nastya.by@mail.ru>
2021-08-18 12:02:59 +0300

    feat: change index file

--- src/index.js
+++ src/index.js
@@ -39,6 +39,23 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result = "";
+    const length = expr.length;
+
+    for (let index = 0; index < length; index += 10) {
+        if (expr[index] === '*') result += ' ';
+        else {
+            let symbol = "",
+                sequenceLength = index + 10;
+
+            for (let position = index; position < sequenceLength; position += 2)
+                if (expr[position] === '1' && expr[position + 1] === '0') symbol += '.';
+                else if (expr[position] === '1' && expr[position + 1] === '1') symbol += '-';
+
+            result += MORSE_TABLE[symbol];
+        }
+    }
+    return result;
 }
 
 module.exports = {
